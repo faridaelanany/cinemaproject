32d2e501f4c66e2e977d600e9e2dade56c73b77e
faridaelanany <159624430+faridaelanany@users.noreply.github.com>
2024-03-08 20:53:34 +0200

    Add files via upload

---
+++ loginwindow.cpp
@@ -0,0 +1,48 @@
+#include "loginwindow.h"
+#include "ui_loginwindow.h"
+#include "Users.h"
+#include "welcomewindow.h"
+#include "registerwindow.h"
+
+LoginWindow::LoginWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::LoginWindow)
+{
+    ui->setupUi(this);
+    ui->error->setVisible(false);
+}
+
+LoginWindow::~LoginWindow()
+{
+    delete ui;
+}
+
+
+void LoginWindow::on_login_clicked()
+{
+    bool SuccessfulLogin = false;
+    for (int i=0; i<usersCount; i++)
+    {
+        if ((usernames[i] == ui->Username->text()) && (passwords[i] == ui->Password->text()))
+        {
+            hide();
+            WelcomeWindow* welcomewindow = new WelcomeWindow(usernames[i], ages[i]);
+            welcomewindow->show();
+
+            SuccessfulLogin = true;
+        }
+    }
+
+    if (SuccessfulLogin == false)
+        ui->error->setVisible(true);
+}
+
+
+void LoginWindow::on_Register_clicked()
+{
+    hide();
+    RegisterWindow* registerWindow = new RegisterWindow(this);
+    registerWindow->show();
+}
+
+
